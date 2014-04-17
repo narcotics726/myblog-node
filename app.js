fde5564a827b44458d8d8ca38b8bc99a4a35f8e1
NarK <narcotics726@gmail.com>
2014-04-18 01:05:04 +0800

    import underscore and underscore.string

--- app.js
+++ app.js
@@ -1,28 +1,11 @@
 var express = require('express');
 var markdown = require('markdown').markdown;
 var fs = require('fs');
-var webconfig = require('./webconfig');
-
+var _ = require('underscore');
+_.str = require('underscore.string');
 var app = express();
 
-/** @method pad
-@param num {int} the number need padding
-@param n {int} padding length
-@return {string} number after padding
-@example
-var str = pad(1, 2);
-//str = 01
-var str = pad(888, 2);
-//str = 888
-**/
-function pad(num, n) {
-  var len = num.toString().length;
-  while (len < n) {
-    num = "0" + num;
-    len++;
-  }
-  return num;
-}
+var webconfig = require('./webconfig');
 
 /** @method getBlogList
 @param files {array} array of file names(no path, and in specific form like '2014-01-10-blogname.md')
@@ -33,7 +16,7 @@ function getBlogList(files, callback) {
     var blogList = [];
     console.log(files);
     files.forEach(function (filename) {
-      var regPattern = /^[0-9][0-9][0-9][0-9]-[0-1][0-9]-[0-9][0-9]-.{1,}\.md$/;
+      var regPattern = /^[0-9]{4}-[0-1][0-9]-[0-9]{2}-.{1,}\.md$/;
       if (filename.search(regPattern) !== -1) {
         var blogItem = {};
         blogItem.dateStr = filename.substring(0, 10);
@@ -41,7 +24,7 @@ function getBlogList(files, callback) {
         blogItem.blogdate = new Date();
         blogItem.blogdate.setTime(Date.parse(blogItem.dateStr));
         blogItem.dateYear = blogItem.blogdate.getFullYear();
-        blogItem.dateMonth = pad(blogItem.blogdate.getMonth() + 1, 2);
+        blogItem.dateMonth = _.str.pad(blogItem.blogdate.getMonth() + 1, 2, '0');
         blogItem.dateDate = blogItem.blogdate.getDate();
         blogList.push(blogItem);
       } else {
@@ -50,11 +33,11 @@ function getBlogList(files, callback) {
     });
     return callback(null, blogList);
   } catch (err) {
-    return callback(err, null);
+    return callback(err);
   }
 }
 
-app.use(function (req, res, next){
+app.use(function (req, res, next) {
   console.log(req.url);
   next();
 });
@@ -67,7 +50,9 @@ blogRouter.use(function (req, res, next) {
 
 blogRouter.get('/:year/:month/:day/:title', function (req, res, next) {
   console.log(req.params.title);
-  var filename = './blog/' + req.params.year + '-' + req.params.month + '-' + req.params.day + '-' + req.params.title + '.md';
+  var filename = 
+    './blog/' + req.params.year + '-' + req.params.month + '-' + req.params.day + '-' +
+    req.params.title + '.md';
   console.log(filename);
   fs.exists(filename, function (exists) {
     if (exists) {
@@ -85,13 +70,17 @@ blogRouter.get('/:year/:month/:day/:title', function (req, res, next) {
 
 app.use('/blog', blogRouter);
 
-app.use('/', function (req, res) {
+app.use('/', function (req, res, next) {
   fs.readdir(webconfig.blogdir, function (err, files) {
     if (files && files.length) {
       var html = '';
       getBlogList(files, function (err, blogList) {
+        if (err) {
+          next(err);
+        }
         blogList.forEach(function (blog) {
-          blog.url = '/blog/' + blog.dateYear + '/' + blog.dateMonth + '/' + blog.dateDate + '/' + blog.title;
+          //blog.url = '/blog/' + blog.dateYear + '/' + blog.dateMonth + '/' + blog.dateDate + '/' + blog.title;
+          blog.url = _.str.sprintf('/blog/%(dateYear)s/%(dateMonth)s/%(dateDate)s/%(title)s', blog);
           html += '<a href="' + blog.url + '">' + blog.title + '</a><br />';
         });
         res.send(html);
