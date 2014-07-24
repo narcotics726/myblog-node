560d870bfb5dd9036c07c3c630247a83f112b6c3
Nark <narcotics726@gmail.com>
2014-07-24 23:23:31 +0800

    add dpbxapi url to adminController
    placeholder for later api invoke test entrance

--- controller/adminController.js
+++ controller/adminController.js
@@ -33,4 +33,18 @@ adminRouter.post('/login', function (req, res, next) {
   return res.redirect('/');
 });
 
+adminRouter.get('/dpbxapi/:cmd', function (req, res, next) {
+  switch (req.params.cmd) {
+  case 'getcode':
+    console.log(req.session.user.DropboxCode);
+    break;
+  case 'accountInfo':
+    console.log('accountInfo');
+    var url = "https://api.dropbox.com/1/account/info";
+    break;
+  default:
+    console.log('no such cmd');
+  }
+});
+
 module.exports = adminRouter;
