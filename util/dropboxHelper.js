f72a0cf3fb309b63a99b97521b1e877cda49b8c7
Nark <narcotics726@gmail.com>
2014-07-26 23:42:52 +0800

    add two api to apitable
    create_folder,
    metadata

--- util/dropboxHelper.js
+++ util/dropboxHelper.js
@@ -17,7 +17,21 @@ var apiOptTable = {
     path: '/1/oauth2/token?code=%(code)s&grant_type=%(grant_type)s&client_id=%(client_id)s&client_secret=%(client_secret)s&redirect_uri=%(redirect_uri)s',
     method: 'POST',
     port: 443
-  }
+  },
+  //create folder
+  create_folder: {
+    hostname: 'api.dropbox.com',
+    path: '/1/fileops/create_folder?access_token=%(token)s&root=%(root)s&path=%(path)s&locale=%(locale)s',
+    method: 'POST',
+    port: 443
+  },
+  //metadata
+  metadata: {
+    hostname: 'api.dropbox.com',
+    path: '/1/metadata/auto/$(path)s?access_token=%(token)s&file_limit=%(file_limit)s&hash=%(hash)s&list=%(list)s&include_deleted=%(include_deleted)s&rev=%(rev)s&locale=%(locale)s&include_media_info=%(include_media_info)s',
+    method: 'GET',
+    port: 443
+  },
 };
 
 function getHttpOptions(api, args) {
